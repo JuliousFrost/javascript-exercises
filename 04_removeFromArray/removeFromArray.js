c81fc75af76dcb947a9b84121eb088ce383a82dd
Julious <niloyjulious@gmail.com>
2023-08-15 13:14:48 +0600

    sd

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,15 @@
 const removeFromArray = function(arr, ...theArgs) {
 
     console.log(arr);
+    console.log(theArgs);
 
     for(const i of theArgs){
         if (arr.includes(i)){
-            arr.pull(i)
+            index = arr.indexOf(i);
+            arr.splice(index,1)
         }
     }
-
+    console.log(arr);
     return arr;
 
 };
