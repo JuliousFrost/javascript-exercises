22384a472440c7b151470a3a05c7290df65dfcd6
Julious <niloyjulious@gmail.com>
2023-08-16 22:43:22 +0600

    Fixed all issues

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
+    console.log("🚀 ~ file: sumAll.js:2 ~ sumAll ~ int2:", int2)
+    console.log("🚀 ~ file: sumAll.js:2 ~ sumAll ~ int1:", int1)
+    let total = 0;
+    console.log("🚀 ~ file: sumAll.js:5 ~ sumAll ~ total:", total)
+
+    if(typeof(int1)!= "number" || int1 < 0 ){
+        return "ERROR"
+    }
+    if(typeof(int2)!= "number" || int2 < 0){
+        return "ERROR"
+    }
+
+    if (int1>int2){
+        const temp = int1;
+        int1 = int2;
+        int2 = temp;
+    }
+
+    while(int1<=int2){
+        total+= int1;
+        int1++;
+    }
+    console.log("Neer is a nub");
+
+    return total;
+
 
 };
 
