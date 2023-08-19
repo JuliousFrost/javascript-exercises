ff13ec75d832fe9eb351cf2d3ceedf791f8bd684
Julious <niloyjulious@gmail.com>
2023-08-19 15:06:36 +0600

    adad

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
+const ftoc = function(cel) {
+
+  let far =  (cel-32) * (5/9) ;
+  return Math.round(far * 10) / 10;
 
 };
 
-const ctof = function() {
+const ctof = function(far2) {
+  let cel2 = far2 * (9/5) + 32;
+  return Math.round(cel2 * 10) / 10;
 
 };
 
