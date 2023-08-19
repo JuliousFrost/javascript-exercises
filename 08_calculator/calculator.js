7aa0dc297337a90f3ea8988057eb351500b20308
Julious <niloyjulious@gmail.com>
2023-08-19 15:40:15 +0600

    adad

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,47 @@
-const add = function() {
+const add = function(a,b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(arr) {
+	let total = 0;
+  for(var i in arr){
+    total = total+arr[i];
+  }
+  return total;
+
+};
+
+const multiply = function(arr) {
+  let total = arr[0];
+  for(var i in arr){
+    total = total*arr[i];
+  }
+  return total/arr[0];
+};
+
+const power = function(x,y) {
+  return Math.pow(x, y);
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  let fac = 1;
+  if(num == 0 || num ==1){
+    return 1;
+}
+else if(num>1){
+  for (var i =num; i>=1; i--){
+    fac = fac*i;
+  }
+  
+}
+return fac;
+}
 
 // Do not edit below this line
 module.exports = {
