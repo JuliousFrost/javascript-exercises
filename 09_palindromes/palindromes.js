83201191841d9d550985771f3fcf5b29fffb3f79
Julious <niloyjulious@gmail.com>
2023-08-19 16:47:43 +0600

    asda

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    const lowerCase = str.toLowerCase();
+    temp = lowerCase.match(/[a-z0-9]/g);
+    alpha = temp.join();
+
+
+    snd = temp.reverse().join();
+    
+    if (alpha===snd){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+
+    
 
 };
 
