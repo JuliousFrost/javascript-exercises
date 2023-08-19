baa31ec1d6959da23bea84b787aaa3e34904fb49
Julious <niloyjulious@gmail.com>
2023-08-19 23:11:01 +0600

    asda

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    l = [];
+    l[0] = 1;
+    l[1] = 1;
+    count = 2;
+    if(num<1){
+        return "OOPS";
+    }
+    while (count<=num){
+        l[count] = l[count -1] + l[count-2];
+        count++;
+
+    }
+    console.log("🚀 ~ file: fibonacci.js:3 ~ fibonacci ~ l:", l)
+
+    return l[num-1];
 
 };
 
